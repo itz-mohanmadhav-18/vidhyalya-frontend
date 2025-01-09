969dbbbf0119e8444c4bf0011fab0e9d99db2214
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-01-09 12:27:56 +0530

    hosting on github-pages completed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/vidhyalya-frontend/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
