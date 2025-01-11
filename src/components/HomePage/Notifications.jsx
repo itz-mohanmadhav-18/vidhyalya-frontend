5b3a74bb9c37ac0edf370f338ef606387590199a
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-01-11 20:59:40 +0530

    notice board fixed

--- src/components/HomePage/Notifications.jsx
+++ src/components/HomePage/Notifications.jsx
@@ -18,16 +18,16 @@ const NotificationBoard = () => {
     <>
       {/* Notification Bell Icon */}
       <div
-        className="fixed hidden-scrollbar bottom-6 right-6 text-3xl cursor-pointer z-50 bg-black text-yellow-300 p-3 px-4 rounded-full shadow-xl hover:scale-110 transition-all duration-300 hover:bg-gray-900"
+        className="fixed hidden-scrollbar bottom-6 right-6 text-3xl cursor-pointer z-50 bg-black text-yellow-300 p-3 px-4 rounded-full shadow-xl hover:scale-110 transition-all duration-300 hover:bg-gray"
         onClick={toggleBoard}
       >
-        <i className="fas fa-bell animate-bounce"></i>
+        <i className="fas fa-bell animate-pulse "></i>
       </div>
 
       {/* Blackboard Notification Board */}
       {isVisible && (
         <div
-          className="fixed bottom-24 w-[350px] h-[350px] right-6 sm:w-[400px] sm:h-[360px] overflow-y-auto   bg-[#383838] border-[12px] border-t-[#bda27e] border-l-[#b19876] border-b-[#c9ad86] border-r-[#b19876] rounded-lg z-50 transition-all duration-300 transform scrollbar-thin scrollbar-thumb-gray-800 scrollbar-track-gray-900"
+          className="fixed bottom-24 w-[280px] h-[280px] xs:w-[300px] xs:h-[300px] right-4 sm:right-6 sm:w-[400px] sm:h-[360px] overflow-y-auto   bg-[#383838] border-[12px] border-t-[#bda27e] border-l-[#b19876] border-b-[#c9ad86] border-r-[#b19876] rounded-lg z-50 transition-all duration-300 transform scrollbar-thin scrollbar-thumb-gray-800 scrollbar-track-gray-900"
           style={{
             boxShadow: "0px 0px 6px 5px rgba(58, 18, 13, 0), 0px 0px 0px 2px #c2a782, 0px 0px 0px 4px #a58e6f, 3px 4px 8px 5px rgba(0, 0, 0, 0.5)",
             backgroundImage: 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAYAAACmGA8nAAAAEElEQVQImWP4//8/wygAAgAB/xwVO9YAAAAASUVORK5CYII=")', // Subtle texture
@@ -39,7 +39,7 @@ const NotificationBoard = () => {
             className="relative p-6 text-white overflow-y-auto hidden-scrollbar "
           >
             <h2
-              className="text-3xl font-bold text-center mb-6 text-white hidden-scrollbar tracking-wider"
+              className="text-2xl sm:!text-3xl font-bold text-center mb-6 text-white hidden-scrollbar tracking-wider"
               style={chalkboardText}
             >
               <i className="fas fa-clipboard-list mr-3"></i>
@@ -59,7 +59,7 @@ const NotificationBoard = () => {
               ].map((item, index) => (
                 <li
                   key={index}
-                  className="flex items-center py-2 px-4 rounded-md hover:bg-gray-800 transition-colors duration-300"
+                  className="flex items-center py-0 px-2 rounded-md hover:bg-gray-800 transition-colors duration-300"
                 >
                   <span
                     className="text-yellow-400 mr-4 text-xl"
