20062511d9424bf34d8d535edf90cc222fc3ae64
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-01-12 10:50:14 +0530

    map pin changed to gla university

--- src/components/HomePage/Footer.jsx
+++ src/components/HomePage/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from '../../assets/logo.png';
+import logo from "../../assets/logo.png";
 
 const Footer = ({ openContactForm }) => {
   return (
@@ -50,24 +50,23 @@ const Footer = ({ openContactForm }) => {
 
       {/* Main Footer Content */}
       <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-8">
-
         {/* Google Map Section */}
         <div className="flex justify-center md:justify-start">
           <iframe
             title="Location Map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.0196461014557!2d-122.7061924!3d38.3458328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808448fef35f299d%3A0xd7b382a8b03ba788!2s7165%20Burton%20Ave%2C%20Rohnert%20Park%2C%20CA%2094928%2C%20USA!5e0!3m2!1sen!2sin!4v1614005128506!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d204772.16739471912!2d77.29216469453125!3d27.604857699999993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39736ce47bffc039%3A0xfe5fc3da92c6341!2sGLA%20University!5e1!3m2!1sen!2sin!4v1736658656806!5m2!1sen!2sin&zoomControl=0"
             width="300"
             height="220"
             style={{ border: 0 }}
             allowFullScreen=""
             loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
 
-
         {/* Contact Section */}
         <div className="flex flex-col items-center md:items-start">
-          <button 
+          <button
             onClick={openContactForm}
             className="text-lg font-semibold hover:text-light-brown underline"
           >
@@ -79,10 +78,7 @@ const Footer = ({ openContactForm }) => {
           </address>
           <p className="mt-2 text-sm">P: 707-792-4700</p>
           <p className="text-sm">F: 707-792-4537</p>
-          <a
-            href="#"
-            className="mt-9 text-lg underline hover:text-light-brown"
-          >
+          <a href="#" className="mt-9 text-lg underline hover:text-light-brown">
             Career Opportunities
           </a>
         </div>
@@ -90,22 +86,13 @@ const Footer = ({ openContactForm }) => {
         {/* Links Section */}
         <div className="flex flex-row justify-center md:justify-start gap-4">
           <div className="flex flex-col space-y-3 text-md">
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               District Resources
             </a>
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               Careers
             </a>
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               Use of Facilities
             </a>
             <button
@@ -114,48 +101,27 @@ const Footer = ({ openContactForm }) => {
             >
               Contact Us
             </button>
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               Uniform Complaint Procedure
             </a>
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               Title IX
             </a>
           </div>
           <div className="flex flex-col space-y-3 text-md">
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               Find Your School
             </a>
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               School Locations Map
             </a>
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               Family Resources
             </a>
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               School Calendar
             </a>
-            <a
-              href="#"
-              className="hover:text-light-brown"
-            >
+            <a href="#" className="hover:text-light-brown">
               Enrollment Information
             </a>
           </div>
