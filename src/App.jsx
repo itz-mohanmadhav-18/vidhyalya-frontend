8a901aed92dc72b11da7ef283c722f4b68ba0d8a
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-01-23 21:55:08 +0530

    footer links

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import UnderDevelopment from './components/OtherPages/underDevelopment';
 import CareerPage from './components/CareerPage';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Disclosure from "./components/Disclosure";
+import Copyright from "./components/OtherPages/Copyright";
+import Disclaimer from "./components/OtherPages/Disclaimer";
+import Privacy from "./components/OtherPages/Privacy";
+import Terms from "./components/OtherPages/TermsAndConditions";
+import Hyperlink from "./components/OtherPages/Hyperlinks";
 
 
 function App() {
@@ -20,6 +25,11 @@ function App() {
         <Route path="/admissions" element={<AdmissionPage />} />
         <Route path="/dashboard" element={<UnderDevelopment />} />
         <Route path="/disclosure" element={<Disclosure />} />
+        <Route path="copyright" element={<Copyright />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/hyperlink" element={<Hyperlink />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
